9d5f72c16412c0c54607d679d56e728bbde29339
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-26 14:05:09 -0500

    Create Dense Layers.py

---
+++ Dense Layers.py
@@ -0,0 +1,11 @@
+# Load layers
+from keras.layers import Input, Dense
+
+# Input layer
+input_tensor = Input(shape=(1,))
+
+# Dense layer
+output_layer = Dense(1)
+
+# Connect the dense layer to the input_tensor
+output_tensor = output_layer(input_tensor)
