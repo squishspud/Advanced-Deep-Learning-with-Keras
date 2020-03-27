3711263adacbd7cd95df834113561b9944ad8026
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-27 14:32:56 -0500

    Create Evaluate the model on a test set.py

---
+++ Evaluate the model on a test set.py
@@ -0,0 +1,8 @@
+# Load the X variable from the test data
+X_test = games_tourney_test['seed_diff']
+
+# Load the y variable from the test data
+y_test = games_tourney_test['score_diff']
+
+# Evaluate the model on the test data
+print(model.evaluate(X_test, y_test, verbose=False))
