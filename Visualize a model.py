580f7debe0b88293ba3895fb814cc3f885fa3218
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-27 14:28:02 -0500

    Create Visualize a model.py

---
+++ Visualize a model.py
@@ -0,0 +1,14 @@
+# Import the plotting function
+from keras.utils import plot_model
+import matplotlib.pyplot as plt
+
+# Summarize the model
+model.summary()
+
+# Plot the model
+plot_model(model, to_file='model.png')
+
+# Display the image
+data = plt.imread('model.png')
+plt.imshow(data)
+plt.show()
