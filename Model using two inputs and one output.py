42ea23de413128f6015ca03f123c9a5a934f8528
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-29 15:06:49 -0500

    Create Model using two inputs and one output.py

---
+++ Model using two inputs and one output.py
@@ -0,0 +1,12 @@
+# Imports
+from keras.layers import Subtract
+from keras.models import Model
+
+# Subtraction layer from previous exercise
+score_diff = Subtract()([team_1_strength, team_2_strength])
+
+# Create the model
+model = Model([team_in_1, team_in_2], score_diff)
+
+# Compile the model
+model.compile('adam', 'mean_absolute_error')
