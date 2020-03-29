5aaa9807432e0536eb3f6aa48e709a2df5c2ac74
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-29 12:54:44 -0500

    Create Define team lookup.py

---
+++ Define team lookup.py
@@ -0,0 +1,12 @@
+# Imports
+from keras.layers import Embedding
+from numpy import unique
+
+# Count the unique number of teams
+n_teams = unique(games_season['team_1']).shape[0]
+
+# Create an embedding layer
+team_lookup = Embedding(input_dim=n_teams,
+                        output_dim=1,
+                        input_length=1,
+                        name='Team-Strength')
