30534135b0892f4e89ec377f7d7ca42d35435cc6
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-03-29 14:54:00 -0500

    Create Lookup both inputs in the same model.py

---
+++ Lookup both inputs in the same model.py
@@ -0,0 +1,5 @@
+# Lookup team 1 in the team strength model
+team_1_strength = team_strength_model(team_in_1)
+
+# Lookup team 2 in the team strength model
+team_2_strength = team_strength_model(team_in_2)
