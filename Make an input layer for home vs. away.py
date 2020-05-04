ed1219629ce829fb162208379daad865580e65c8
squish mellow <37819271+squishspud@users.noreply.github.com>
2020-05-04 12:19:03 -0500

    Create Make an input layer for home vs. away.py

---
+++ Make an input layer for home vs. away.py
@@ -0,0 +1,14 @@
+# Create an Input for each team
+team_in_1 = Input(shape=(1,), name='Team-1-In')
+team_in_2 = Input(shape=(1,), name='Team-2-In')
+
+# Create an input for home vs away
+home_in = Input(shape=(1,), name='Home-In')
+
+# Lookup the team inputs in the team strength model
+team_1_strength = team_strength_model(team_in_1)
+team_2_strength = team_strength_model(team_in_2)
+
+# Combine the team strengths with the home input using a Concatenate layer, then add a Dense layer
+out = Concatenate()([team_1_strength, team_2_strength, home_in])
+out = Dense(1)(out)
